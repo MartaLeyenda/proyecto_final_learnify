03d51d3f1f1372c60db1a802bfb5cd2bb3f9743d
MartaLeyenda <martaleyendaa@gmail.com>
2025-04-27 13:57:44 +0200

    Añadir proyecto

---
+++ proyecto_final_learnify/Form2.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace proyecto_final_learnify
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.lblApellidos = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.numEdad = new System.Windows.Forms.NumericUpDown();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblGmail = new System.Windows.Forms.Label();
+            this.txtGmail = new System.Windows.Forms.TextBox();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.calendarioFecha = new System.Windows.Forms.MonthCalendar();
+            this.lblContrasenna = new System.Windows.Forms.Label();
+            this.txtContrasenna = new System.Windows.Forms.TextBox();
+            this.lblContrasenna2 = new System.Windows.Forms.Label();
+            this.txtContrasenna2 = new System.Windows.Forms.TextBox();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.cbTipo = new System.Windows.Forms.ComboBox();
+            this.btnRegistrarse = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numEdad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(13, 13);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(59, 16);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(75, 12);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(284, 22);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(79, 40);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(280, 22);
+            this.txtApellidos.TabIndex = 3;
+            // 
+            // lblApellidos
+            // 
+            this.lblApellidos.AutoSize = true;
+            this.lblApellidos.Location = new System.Drawing.Point(13, 41);
+            this.lblApellidos.Name = "lblApellidos";
+            this.lblApellidos.Size = new System.Drawing.Size(67, 16);
+            this.lblApellidos.TabIndex = 2;
+            this.lblApellidos.Text = "Apellidos:";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Location = new System.Drawing.Point(13, 69);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(43, 16);
+            this.lblEdad.TabIndex = 4;
+            this.lblEdad.Text = "Edad:";
+            // 
+            // numEdad
+            // 
+            this.numEdad.Location = new System.Drawing.Point(59, 67);
+            this.numEdad.Name = "numEdad";
+            this.numEdad.Size = new System.Drawing.Size(300, 22);
+            this.numEdad.TabIndex = 5;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(13, 99);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(106, 16);
+            this.lblUsuario.TabIndex = 6;
+            this.lblUsuario.Text = "Nombre usuario:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(128, 95);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(231, 22);
+            this.txtUsuario.TabIndex = 7;
+            // 
+            // lblGmail
+            // 
+            this.lblGmail.AutoSize = true;
+            this.lblGmail.Location = new System.Drawing.Point(13, 125);
+            this.lblGmail.Name = "lblGmail";
+            this.lblGmail.Size = new System.Drawing.Size(45, 16);
+            this.lblGmail.TabIndex = 8;
+            this.lblGmail.Text = "Gmail:";
+            // 
+            // txtGmail
+            // 
+            this.txtGmail.Location = new System.Drawing.Point(65, 125);
+            this.txtGmail.Name = "txtGmail";
+            this.txtGmail.Size = new System.Drawing.Size(294, 22);
+            this.txtGmail.TabIndex = 9;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(13, 157);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(116, 16);
+            this.lblFecha.TabIndex = 10;
+            this.lblFecha.Text = "Fecha nacimiento:";
+            // 
+            // calendarioFecha
+            // 
+            this.calendarioFecha.Location = new System.Drawing.Point(139, 157);
+            this.calendarioFecha.Name = "calendarioFecha";
+            this.calendarioFecha.TabIndex = 11;
+            // 
+            // lblContrasenna
+            // 
+            this.lblContrasenna.AutoSize = true;
+            this.lblContrasenna.Location = new System.Drawing.Point(13, 375);
+            this.lblContrasenna.Name = "lblContrasenna";
+            this.lblContrasenna.Size = new System.Drawing.Size(79, 16);
+            this.lblContrasenna.TabIndex = 12;
+            this.lblContrasenna.Text = "Contraseña:";
+            // 
+            // txtContrasenna
+            // 
+            this.txtContrasenna.Location = new System.Drawing.Point(101, 372);
+            this.txtContrasenna.Name = "txtContrasenna";
+            this.txtContrasenna.PasswordChar = '*';
+            this.txtContrasenna.Size = new System.Drawing.Size(258, 22);
+            this.txtContrasenna.TabIndex = 13;
+            // 
+            // lblContrasenna2
+            // 
+            this.lblContrasenna2.AutoSize = true;
+            this.lblContrasenna2.Location = new System.Drawing.Point(13, 403);
+            this.lblContrasenna2.Name = "lblContrasenna2";
+            this.lblContrasenna2.Size = new System.Drawing.Size(150, 16);
+            this.lblContrasenna2.TabIndex = 14;
+            this.lblContrasenna2.Text = "Confirme su contraseña:";
+            // 
+            // txtContrasenna2
+            // 
+            this.txtContrasenna2.Location = new System.Drawing.Point(163, 400);
+            this.txtContrasenna2.Name = "txtContrasenna2";
+            this.txtContrasenna2.PasswordChar = '*';
+            this.txtContrasenna2.Size = new System.Drawing.Size(196, 22);
+            this.txtContrasenna2.TabIndex = 15;
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(13, 431);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(97, 16);
+            this.lblTipo.TabIndex = 16;
+            this.lblTipo.Text = "Tipo de usario:";
+            // 
+            // cbTipo
+            // 
+            this.cbTipo.FormattingEnabled = true;
+            this.cbTipo.Items.AddRange(new object[] {
+            "---------",
+            "Profesor",
+            "Alumno"});
+            this.cbTipo.Location = new System.Drawing.Point(119, 428);
+            this.cbTipo.Name = "cbTipo";
+            this.cbTipo.Size = new System.Drawing.Size(240, 24);
+            this.cbTipo.TabIndex = 17;
+            // 
+            // btnRegistrarse
+            // 
+            this.btnRegistrarse.Location = new System.Drawing.Point(16, 458);
+            this.btnRegistrarse.Name = "btnRegistrarse";
+            this.btnRegistrarse.Size = new System.Drawing.Size(343, 35);
+            this.btnRegistrarse.TabIndex = 18;
+            this.btnRegistrarse.Text = "REGISTRARSE";
+            this.btnRegistrarse.UseVisualStyleBackColor = true;
+            this.btnRegistrarse.Click += new System.EventHandler(this.btnRegistrarse_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 505);
+            this.Controls.Add(this.btnRegistrarse);
+            this.Controls.Add(this.cbTipo);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.txtContrasenna2);
+            this.Controls.Add(this.lblContrasenna2);
+            this.Controls.Add(this.txtContrasenna);
+            this.Controls.Add(this.lblContrasenna);
+            this.Controls.Add(this.calendarioFecha);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.txtGmail);
+            this.Controls.Add(this.lblGmail);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.numEdad);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.lblApellidos);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.Text = "REGISTRARSE";
+            ((System.ComponentModel.ISupportInitialize)(this.numEdad)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.Label lblApellidos;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.NumericUpDown numEdad;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblGmail;
+        private System.Windows.Forms.TextBox txtGmail;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.MonthCalendar calendarioFecha;
+        private System.Windows.Forms.Label lblContrasenna;
+        private System.Windows.Forms.TextBox txtContrasenna;
+        private System.Windows.Forms.Label lblContrasenna2;
+        private System.Windows.Forms.TextBox txtContrasenna2;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.ComboBox cbTipo;
+        private System.Windows.Forms.Button btnRegistrarse;
+    }
+}
---
+++ proyecto_final_learnify/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyecto_final_learnify
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrarse_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text.Trim() == "" || txtApellidos.Text.Trim() == "" || txtUsuario.Text.Trim() == "" || txtGmail.Text.Trim() == "" || txtContrasenna.Text.Trim() == "" || txtContrasenna2.Text.Trim() == "" || numEdad.Value <= 0)
+            {
+                MessageBox.Show("Alguno de los datos está vacío o la edad es incorrecta", "ERROR!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else
+            {
+                // Guardar datos en la base de datos
+            }
+        }
+    }
+}
